2f1c4f7c2b15f9b2a448004affc1e9f8de17811c
Refrain57 <Tanyx57@163.com>
2025-11-12 10:34:39 +0800

    重力概率成像

---
+++ MapGISPlugin3/重磁处理功能/Form_GravityCorrelationImaging.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace MapGISPlugin3
+{
+    // **************************************************
+    // 4. Designer 文件中的 partial class 名已修改
+    // **************************************************
+    partial class Form_GravityCorrelationImaging
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.treeViewLayers = new System.Windows.Forms.TreeView();
+            this.textBoxSavePath = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxGdbDirectory = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(402, 338);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(98, 34);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "确认";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(614, 338);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(98, 34);
+            this.btnCancel.TabIndex = 4;
+            this.btnCancel.Text = "取消";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // treeViewLayers
+            // 
+            this.treeViewLayers.Location = new System.Drawing.Point(68, 22);
+            this.treeViewLayers.Name = "treeViewLayers";
+            this.treeViewLayers.Size = new System.Drawing.Size(216, 349);
+            this.treeViewLayers.TabIndex = 5;
+            this.treeViewLayers.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeViewLayers_AfterSelect);
+            // 
+            // textBoxSavePath
+            // 
+            this.textBoxSavePath.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxSavePath.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBoxSavePath.Location = new System.Drawing.Point(478, 272);
+            this.textBoxSavePath.Name = "textBoxSavePath";
+            this.textBoxSavePath.ReadOnly = true;
+            this.textBoxSavePath.Size = new System.Drawing.Size(234, 28);
+            this.textBoxSavePath.TabIndex = 8;
+            this.textBoxSavePath.TextChanged += new System.EventHandler(this.textBoxGdbDirectory_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(334, 276);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 18);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "结果输出命名";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBoxGdbDirectory
+            // 
+            this.textBoxGdbDirectory.Location = new System.Drawing.Point(470, 224);
+            this.textBoxGdbDirectory.Name = "textBoxGdbDirectory";
+            this.textBoxGdbDirectory.Size = new System.Drawing.Size(242, 28);
+            this.textBoxGdbDirectory.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(336, 229);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 18);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "GDB文件夹";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form_GravityCorrelationImaging
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBoxGdbDirectory);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxSavePath);
+            this.Controls.Add(this.treeViewLayers);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Name = "Form_GravityCorrelationImaging";
+            this.Text = "磁相关成像";
+            this.Load += new System.EventHandler(this.Form_MagCorrelationImaging_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.TreeView treeViewLayers;
+        private System.Windows.Forms.TextBox textBoxSavePath;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxGdbDirectory;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ MapGISPlugin3/重磁处理功能/GravityCorrelationImagingCommand.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms; // 确保引用了该命名空间
+using MapGIS.PluginEngine;
+
+namespace MapGISPlugin3
+{
+    /// <summary>
+    /// 这是“磁相关成像”功能的插件入口命令。
+    /// MapGIS平台会识别这个类，并将其作为一个功能按钮或菜单项。
+    /// </summary>
+    public class GravityCorrelationImagingCommand : ICommand
+    {
+        // 用于存储从MapGIS主程序传递过来的IApplication钩子
+        private IApplication _hk;
+
+        #region ICommand 接口成员实现
+
+        /// <summary>
+        /// 按钮的图标（这里暂时不提供，返回null）
+        /// </summary>
+        public Bitmap Bitmap { get { return null; } }
+
+        /// <summary>
+        /// 功能的标题，会显示在按钮或菜单上
+        /// </summary>
+        public string Caption { get { return "重力相关成像"; } }
+
+        /// <summary>
+        /// 功能所属的类别，用于在自定义界面时进行分组
+        /// </summary>
+        public string Category { get { return "地学算法"; } } // 您可以自定义，例如 "MapGISPlugin3"
+
+        /// <summary>
+        /// 对于可切换状态的按钮，表示是否被选中（这里固定为false）
+        /// </summary>
+        public bool Checked { get { return false; } }
+
+        /// <summary>
+        /// 表示按钮是否可用（这里固定为true）
+        /// </summary>
+        public bool Enabled { get { return true; } }
+
+        /// <summary>
+        /// 当鼠标悬停在功能上时，在状态栏显示的详细信息
+        /// </summary>
+        public string Message { get { return "执行重力数据的互相关成像算法"; } }
+
+        /// <summary>
+        /// 功能的唯一编程名称
+        /// </summary>
+        public string Name { get { return "MagCorrelationImagingCommand"; } }
+
+        /// <summary>
+        /// 鼠标悬停时显示的工具提示
+        /// </summary>
+        public string Tooltip { get { return "重力相关成像"; } }
+
+        /// <summary>
+        /// 【核心】当用户点击此功能按钮时执行的代码
+        /// </summary>
+        public void OnClick()
+        {
+            // 确保已经成功获取到了MapGIS应用程序的钩子
+            if (_hk != null)
+            {
+                // 创建我们之前修改好的窗体实例
+                // 并将钩子 _hk 传递给窗体的构造函数
+                Form_GravityCorrelationImaging form = new Form_GravityCorrelationImaging(_hk);
+
+                // 以模态对话框的形式显示窗体
+                form.ShowDialog();
+            }
+            else
+            {
+                MessageBox.Show("未能获取到MapGIS应用程序实例！", "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        /// <summary>
+        /// 当插件被MapGIS加载时，此方法会被调用
+        /// </summary>
+        /// <param name="hook">MapGIS主程序的IApplication接口实例</param>
+        public void OnCreate(IApplication hook)
+        {
+            // 将传递进来的钩子保存到私有变量 _hk 中，以备OnClick时使用
+            this._hk = hook;
+        }
+
+        #endregion
+    }
+}
